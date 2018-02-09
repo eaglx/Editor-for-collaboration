a683f2b9257e64fe8cdc5c1027d8d0c5fb21bd82
inf127239 <jakub.zdanowski@student.put.poznan.pl>
2018-02-09 16:56:18 +0100

    Add diffSearch

--- client/mainwindow.cpp
+++ client/mainwindow.cpp
@@ -17,12 +17,47 @@ void MainWindow::internalMessage(int internalFlag)
     connect(ui->textEdit, SIGNAL(textChanged()), this, SLOT(onTextChanged()));
 }
 
+void diffSearch(int len)
+{
+    for(int i = 0; i < len; i++) {
+        if(dataFromServer[i] != dataFromQTextEdit[i]) {
+            //replace(dataFromQTextEdit[i]);      //TODO
+        }
+    }
+}
+
 void MainWindow::onTextChanged()
 {
     /* Code that executes on text change here */
     // QTextCursor  cursor = ui->textEdit->textCursor(); // Get cursor position
     dataFromQTextEdit = ui->textEdit->toPlainText().toUtf8().constData();
     qDebug() << QString::fromStdString(dataFromQTextEdit);
+
+    int lenServer = dataFromServer.length();
+    int lenQText = dataFromQTextEdit.length();
+
+    //change one char
+    if(lenServer == lenQText) {
+        diffSearch(lenServer);
+    }
+
+    //add new chars
+    if(lenQText > lenServer) {
+        diffSearch(lenServer);
+
+        for(int i = lenServer; i < lenQText; i++) {
+            //append(dataFromQTextEdit[i]);   //TODO
+        }
+    }
+
+    //delete chars
+    if(lenServer > lenQText) {
+        diffSearch(lenQText);
+        for(int i = lenQText; i < lenServer; i++) {
+             //delete_(i);            //TODO
+        }
+    }
+
 }
 
 MainWindow::MainWindow(QWidget *parent) :
