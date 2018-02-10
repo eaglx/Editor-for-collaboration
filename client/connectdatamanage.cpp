8322deff3907f2677309862189996b99f80d9057
inf127239 <jakub.zdanowski@student.put.poznan.pl>
2018-02-11 00:43:33 +0100

    Fix RM

--- client/connectdatamanage.cpp
+++ client/connectdatamanage.cpp
@@ -59,7 +59,8 @@ void listen_from_server(MainWindow *w)
             }
             else if(msg.flag == FLAG_RM)
             {
-                dataFromServer = dataFromServer.substr(0, (dataFromServer.size() - 1));
+                if(dataFromServer.length() > 0)
+                    dataFromServer = dataFromServer.substr(0, (dataFromServer.size() - 1));
                 //if(dataFromServer.length() == 0)
                 //    dataFromServer = "#Hey#";
             }
